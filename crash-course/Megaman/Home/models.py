3aed72de9e1720df3197b116ed185fccc4c7e806
Hari om singh <www.hariom03.com@gmail.com>
2023-08-29 17:23:03 +0530

    add models into our app and create superuser to login in admin

--- crash-course/Megaman/Home/models.py
+++ crash-course/Megaman/Home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Data(models.Model):
+    text = models.TextField(max_length=255)
+    name = models.CharField(max_length=155)
+    age = models.IntegerField(max_length=3)
+
+    def __str__(self) -> str:
+        return self.name
