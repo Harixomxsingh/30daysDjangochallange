258eb23060911574859a8335dfff4ce136affffc
Hari om singh <www.hariom03.com@gmail.com>
2023-09-17 13:16:54 +0530

    now i install right and tested dotenv

--- blog-project/djblogger/djblogger/settings/base.py
+++ blog-project/djblogger/djblogger/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+
 # from decouple import config
 from dotenv import load_dotenv
 import os
@@ -25,11 +26,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = config('SECRET_KEY')
-SECRET_KEY = os.environ[SECRET_KEY]
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.environ[DEBUG]
+DEBUG = os.environ.get("DEBUG")
 ALLOWED_HOSTS = []
 
 
