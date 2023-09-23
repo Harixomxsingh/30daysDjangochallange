029eb85bb15e804d542a1c2934672ff28c414d11
Hari om singh <www.hariom03.com@gmail.com>
2023-09-24 00:01:45 +0530

    infinite scroll using htmx

--- blog-project/djblogger/blog/views.py
+++ blog-project/djblogger/blog/views.py
@@ -5,7 +5,15 @@ from django.views.generic import ListView
 
 # Create your views here.
 
+
 class HomeView(ListView):
     model = Post
     template_name = "blog/index.html"
     context_object_name = "posts"
+    paginate_by = 5
+
+    def get_template_names(self):
+        if self.request.htmx:
+            print("htmx works!")
+            return "blog/components/bloglist.html"
+        return "blog/index.html"
